1680358f33b95122b9ede9a248b569ae703838bd
park-yina <kln99988@gmail.com>
2024-05-13 12:31:30 +0900

    소스 변경

--- include/devices/timer.h
+++ include/devices/timer.h
@@ -9,11 +9,6 @@
 
 void timer_init (void);
 void timer_calibrate (void);
-void thread_sleep(int64_t ticks);
-void thread_awake(int64_t ticks);
-void minimum_tick_awake(int64_t ticks);
-int64_t get_next_tick_to_awake(void);
-//위에 추가된 헤더파일 내의 함수들은 스레드를 block->깨어나면 ready로 변경하기 위해 추가한 함수다.
 
 int64_t timer_ticks (void);
 int64_t timer_elapsed (int64_t);
