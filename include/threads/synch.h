721f304c551d2d380e98e4597fbe160bba0548c8
park-yina <kln99988@gmail.com>
2024-05-15 14:45:07 +0900

    testing

--- include/threads/synch.h
+++ include/threads/synch.h
@@ -28,21 +28,12 @@ void sema_up (struct semaphore *);
 void sema_self_test (void);
 
 /* Lock. */
-struct lock {
-	struct thread *holder;      /* Thread holding lock (for debugging). */
-	struct semaphore semaphore; /* Binary semaphore controlling access. */
-};
 
 void lock_init (struct lock *);
 void lock_acquire (struct lock *);
 bool lock_try_acquire (struct lock *);
 void lock_release (struct lock *);
 bool lock_held_by_current_thread (const struct lock *);
-
-/* Condition variable. */
-struct condition {
-	struct list waiters;        /* List of waiting threads. */
-};
 
 void cond_init (struct condition *);
 void cond_wait (struct condition *, struct lock *);
