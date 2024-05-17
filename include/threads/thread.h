c8ab02ee21474fb70d191ef670c16755f3379634
park-yina <kln99988@gmail.com>
2024-05-17 19:35:42 +0900

    fp함수 재정의 수정

--- include/threads/thread.h
+++ include/threads/thread.h
@@ -172,16 +172,4 @@ int thread_get_load_avg(void);
 
 void do_iret(struct intr_frame *tf);
 extern const int f_const = 1<<14;
-
-int int_to_fp (int n);
-int fp_to_int_round (int x);
-int fp_to_int (int x);
-int add_fp (int x, int y);
-int add_mixed (int x, int n);
-int sub_fp (int x, int y);
-int sub_mixed (int x, int n);
-int mult_fp (int x, int y);
-int mult_mixed (int x, int y);
-int div_fp (int x, int y);
-int div_mixed (int x, int n);
 #endif /* threads/thread.h */
