2bbde5bff38d089176cc111eb5ebc4d12c120509
park-yina <kln99988@gmail.com>
2024-05-17 15:04:33 +0900

    header파일 변경

--- threads/thread.c
+++ threads/thread.c
@@ -11,7 +11,7 @@
 #include "threads/synch.h"
 #include "threads/vaddr.h"
 #include "intrinsic.h"
-#include "fixed_point.h"
+#include "threads/fixed_point.h"
 #ifdef USERPROG
 #include "userprog/process.h"
 #endif
