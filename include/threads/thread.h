c0e0c016883ae0bb5ec599f2ad20fc17004f2dc0
park-yina <kln99988@gmail.com>
2024-05-17 15:15:57 +0900

    헤더 수정

--- include/threads/thread.h
+++ include/threads/thread.h
@@ -85,6 +85,7 @@ typedef int tid_t;
  * only because they are mutually exclusive: only a thread in the
  * ready state is on the run queue, whereas only a thread in the
  * blocked state is on a semaphore wait list. */
+
 struct thread
 {
 	/* Owned by thread.c. */
@@ -168,5 +169,18 @@ int thread_get_recent_cpu(void);
 int thread_get_load_avg(void);
 
 void do_iret(struct intr_frame *tf);
+typedef int FP;
+extern const int f_const = 1<<14;
 
+int int_to_fp (int n);
+int fp_to_int_round (int x);
+int fp_to_int (int x);
+int add_fp (int x, int y);
+int add_mixed (int x, int n);
+int sub_fp (int x, int y);
+int sub_mixed (int x, int n);
+int mult_fp (int x, int y);
+int mult_mixed (int x, int y);
+int div_fp (int x, int y);
+int div_mixed (int x, int n);
 #endif /* threads/thread.h */
--- threads/fixed_point.c
+++ threads/fixed_point.c
@@ -1,3 +1,4 @@
+#include"threads/thread.h"
 #define F (1 << 14)
 #define INT_MAX ((1 << 31) - 1)
 #define INT_MIN (-(1 << 31))
