0293b8482f1fbb48000c992b4bfe654008e81377
park-yina <kln99988@gmail.com>
2024-05-23 19:03:09 +0900

    process.h수정

--- include/userprog/process.h
+++ include/userprog/process.h
@@ -3,12 +3,13 @@
 
 #include "threads/thread.h"
 
-tid_t process_create_initd (const char *file_name);
-tid_t process_fork (const char *name, struct intr_frame *if_);
-int process_exec (void *f_name);
-int process_wait (tid_t);
-void process_exit (void);
-void process_activate (struct thread *next);
-/* ----------- Project2 ----------- */
-void argument_stack(char **arg_list, int cnt, struct intr_frame *if_);
+tid_t process_create_initd(const char *file_name);
+tid_t process_fork(const char *name, struct intr_frame *if_);
+int process_exec(void *f_name);
+int process_wait(tid_t);
+void process_exit(void);
+void process_activate(struct thread *next);
+void argument_stack(char **parse, int count, void **rsp);
+// void argument_stack(char **parse, int count, struct intr_frame *if_);
+
 #endif /* userprog/process.h */
